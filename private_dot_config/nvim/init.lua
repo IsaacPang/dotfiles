8842260bed1b36dc7069fd7a5496bcc0b142e755
Isaac Pang <isaac.pang91@gmail.com>
2022-12-26 17:32:35 +0700

    add neovim init lua start

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,48 @@
+-- [[ Settings options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+
+-- Set colorscheme
+-- USING KITTY COLORSCHEME
+-- vim.o.termguicolors = true
+-- vim.cmd [[colorscheme onedark]]
+
+
+-- [[ Basic Keymaps ]]
+-- Set <space> as leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Remap for dealing with word wraps
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk': 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj': 'j'", { expr = true, silent = true })
+
+-- [[ Highlight on yank ]]
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
