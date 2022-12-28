70957161bd56a1f5193c0acf851e450ade5809f6
Isaac Pang <isaac.pang91@gmail.com>
2022-12-28 11:01:51 +0800

    update nvim configs to allow plugins

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- [[ Plugins ]]
+require('plugins')
+
 -- [[ Settings options ]]
 -- See `:help vim.o`
 
---
+++ private_dot_config/nvim/plugins.lua
@@ -0,0 +1,18 @@
+-- This file can be loaded by calling `lua require('plugins')` from init.vim
+
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+end)
