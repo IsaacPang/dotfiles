713a44be998aac005e93c70e434bd25048001b43
Isaac Pang <isaac.pang91@gmail.com>
2024-03-12 14:13:56 +0900

    commit -m "update keys to ensure oil overwrites netrw"
    CHECK TODO how to do both

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
-  "stevearc/oil.nvim",
-  keys = { { "<leader>o", "<cmd>Oil<cr>", desc = "Oil" } },
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+	"stevearc/oil.nvim",
+	--	keys = { { "<leader>o", "<cmd>Oil<cr>", desc = "Oil" } },
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
