d2f4e2680aba3e21e040cac4f862529aedec1a2b
Isaac Pang <isaac.pang91@gmail.com>
2024-05-09 11:01:49 +1000

    update neovim options for python provider

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.fn.has("macunix") then
+	vim.g.python3_host_prog = "/Users/ispang/.pyenv/versions/neovim-3.9.19/bin/python"
+end
