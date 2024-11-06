d459af7056d9d7859356ea3cb176649a1fa689b8
Isaac Pang <ispang@deloitte.com.au>
2024-11-07 08:06:46 +1100

    add vscode updates

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.oil")
+-- check for vscode usage
+if vim.g.vscode then
+	-- vscode vim
+	require("vsocde.keymaps")
+else
+	require("config.lazy")
+	require("config.oil")
+end
