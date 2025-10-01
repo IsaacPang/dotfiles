74724d1d8b8f8d4948858475c5840f3d535805e8
Isaac Pang <ispang@deloitte.com.au>
2025-10-01 10:16:25 +1000

    update neovim configs for bufferline & mason

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
   {
     "catppuccin/nvim",
-    commit = "1bf0701",
     lazy = true,
     name = "catppuccin",
     opts = {
--- private_dot_config/nvim/lua/plugins/mason-workaround.lua
+++ private_dot_config/nvim/lua/plugins/mason-workaround.lua
@@ -1,6 +1,8 @@
-# workaround for mason breaking changes https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+-- Workaround / pin for mason breaking changes
+-- Ref: https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+-- Revert to the original repos & let LazyVim manage versions (version=false) until upstream fully adapts.
 return {
-   { "mason-org/mason.nvim", version = "^1.0.0" },
-   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+   { "mason-org/mason.nvim",            version = false },
+   { "mason-org/mason-lspconfig.nvim",  version = false },
 }
 
