a5c6267530d88649589975b00fd5303fc04d0552
Isaac Pang <isaac.pang91@gmail.com>
2026-04-22 11:44:37 +1000

    update conform

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,34 +1,33 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      python = { "ruff_fix", "ruff_format" },
-    },
-    formatters = {
-      ruff_fix = {
-        command = function()
-          local cwd = vim.fn.getcwd()
-          local ruff_path = cwd .. "/.venv/bin/ruff"
-          if vim.fn.executable(ruff_path) == 1 then
-            return ruff_path
-          end
-          return "ruff"
-        end,
-        args = { "check", "--fix", "--exit-zero", "-" },
-        stdin = true,
-      },
-      ruff_format = {
-        command = function()
-          local cwd = vim.fn.getcwd()
-          local ruff_path = cwd .. "/.venv/bin/ruff"
-          if vim.fn.executable(ruff_path) == 1 then
-            return ruff_path
-          end
-          return "ruff"
-        end,
-        args = { "format", "-" },
-        stdin = true,
-      },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = function()
+		local function get_ruff_command()
+			local cwd = vim.fn.getcwd()
+			local ruff_path = cwd .. "/.venv/bin/ruff"
+			if vim.fn.executable(ruff_path) == 1 then
+				return ruff_path
+			end
+			return "ruff"
+		end
+
+		local ruff_cmd = get_ruff_command()
+
+		return {
+			formatters_by_ft = {
+				python = { "ruff_fix", "ruff_format" },
+			},
+			formatters = {
+				ruff_fix = {
+					command = ruff_cmd,
+					args = { "check", "--fix", "--exit-zero", "-" },
+					stdin = true,
+				},
+				ruff_format = {
+					command = ruff_cmd,
+					args = { "format", "-" },
+					stdin = true,
+				},
+			},
+		}
+	end,
 }
